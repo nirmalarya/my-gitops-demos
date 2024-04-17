5fc5a9a87178433b81aff1e633f29bf4e51a93a3
Nirmal Arya <2323680+nirmalarya@users.noreply.github.com>
2024-04-17 22:29:31 +0000

    test the pipeline again

--- python-docker-demo/main.py
+++ python-docker-demo/main.py
@@ -2,7 +2,7 @@ import random
 import requests
 from bs4 import BeautifulSoup
 
-# crawl IMDB Top 250 and randomly select a movie from IMDB collection
+# crawl IMDB Top 250 and randomly select a movie
 
 URL = 'http://www.imdb.com/chart/top'
 
