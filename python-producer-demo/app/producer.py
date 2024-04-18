efdc8a3a821894877d59cc617324f15284b9a57d
Nirmal Arya <2323680+nirmalarya@users.noreply.github.com>
2024-04-18 11:45:38 +0000

    python producer demo app changes

--- python-producer-demo/app/producer.py
+++ python-producer-demo/app/producer.py
@@ -1,30 +1,29 @@
-from flask import Flask, request, jsonify
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 from kafka import KafkaProducer
 import json
 
-app = Flask(__name__)
+app = FastAPI()
 
-# Initialize Kafka producer and test EDH
+# Initialize Kafka producer
 producer = KafkaProducer(bootstrap_servers='kfk.awsuse1.tst.edh.int.bayer.com:29300')
 
-# API endpoint to trigger event generation
-@app.route('/trigger-event', methods=['POST'])
-def trigger_event():
-    data = request.get_json()
+class WebEvent(BaseModel):
+    user_id: str
+    event_type: str
+    page_id: str
+    referrer: str
+    user_agent: str
 
-    # Generate event based on data received
-    event = {
-        'user_id': data.get('user_id'),
-        'event_type': data.get('event_type'),
-        'page_id': data.get('page_id'),
-        'referrer': data.get('referrer'),
-        'user_agent': data.get('user_agent'),
-    }
+@app.post("/app-pythonproducer-demo/")
+async def trigger_event(web_event: WebEvent):
+    event_data = web_event.dict()
 
     # Send event to Kafka topic
-    producer.send('app.ph-commercial.website.click.events.avro', json.dumps(event).encode('utf-8'))
+    producer.send('app.ph-commercial.website.click.events.avro', json.dumps(event_data).encode('utf-8'))
 
-    return jsonify({'message': 'Event triggered successfully'}), 200
+    return {"message": "Event triggered successfully"}
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=5000)
